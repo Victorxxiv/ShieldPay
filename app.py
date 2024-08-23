3f175cafc2bc0cffece5317ae2f35b8ecd1e7bbe
Victor <victormwendwa804@gmail.com>
2024-08-23 11:02:35 +0300

    Testing Backend API Endpoints

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 """
 Flask App sends and accept json api requests to the set frontend
 """
-from datetime import timedelta
+from datetime import datetime, timedelta
+from functools import wraps
+import uuid
 from flask import Flask, jsonify, make_response, request
+from flask_sqlalchemy import SQLAlchemy  # Database ORM
 import os
-
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
 from db import storage
 from werkzeug.exceptions import HTTPException
@@ -28,7 +31,15 @@ load_dotenv()
 
 
 app = Flask(__name__)
+# Configurations
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('PG_URL', 'sqlite:///Database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database
+db = SQLAlchemy(app)
+
+
 broker_url = os.getenv('CELERY_BROKER_URL')
 celery = Celery(
     app.import_name,
@@ -43,6 +54,9 @@ app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=30)
 app.config['JWT_COOKIE_SECURE'] = True
 app.config['JWT_COOKIE_CSRF_PROTECT'] = True
 app.config['JWT_COOKIE_SAMESITE'] = "None"
+app.config['JSON_SORT_KEYS'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 
 
 
@@ -93,9 +107,152 @@ def health():
 #   return redirect('/api/v1/views/login')
 
 
+# User model and database ORM
+class User(db.Model):
+    # User table
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(110), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+
+# Decorator for token required
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # Check if toke is in the header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # If token is not found  return 401
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            # Decode the token
+            data = jwt.decode(
+                token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = User.query\
+                .filter_by(public_id=data['public_id']).first()
+        except Exception as e:
+            print(e)  # For debugging
+            return jsonify({'message': 'Token is invalid!'}), 401
+        # Return the current user
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+#@app.route('/home')  # Home route
+#def home():
+#    return render_template('home.html')
+
+#@app.route('/about')
+#def about():
+#    return render_template('about.html')
+
+# Route to register users
+@app.route('/user', methods=['GET'])
+@token_required
+def get_all_users(current_user):
+    # Query all users
+    users = User.query.all()
+    # Convert the users to a list of jsons and return
+    output = []
+    for user in users:
+        # Append user to the output list
+        output.append({
+            'public_id': user.public_id,
+            'name': user.name,
+            'email': user.email
+        })
+
+    # Return the list of users
+    return jsonify({'users': output})
+
+
+# Route for user login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Get the request data dict
+    auth = request.form
+    # Check if the request has email and password
+    if not auth or not auth.get('email') or not auth.get('password'):
+        # Return 401 error if they are not found
+        return make_response(
+            'Could not verify', 401,
+            {'WWW-Authenticate': 'Basic realm="Login required!"'}
+        )
+
+    # Query the user with the email
+    user = User.query.filter_by(email=auth.get('email')).first()
+    # If user is not found return 401
+    if not user:
+        return make_response(
+            'Could not verify', 401,
+            {'WWW-Authenticate': 'Basic realm="User not found!"'}
+        )
+
+    # If user is found, check if the password is correct
+    if check_password_hash(user.password, auth.get('password')):
+        # Generate token
+        token = jwt.encode({
+            'public_id': user.public_id,
+            'exp': datetime.utcnow() + timedelta(minutes=30)
+        }, app.config['SECRET_KEY'])
+        # Return the token
+        return jsonify({'token': token}), 201  # 201 if login is successful
+
+    # Return 403 if passsword is incorrect
+    return make_response(
+        'Could not verify', 403,
+        {'WWW-Authenticate': 'Basic realm="Password incorrect!"'}
+    )
+
+
+# Route for register page
+#@app.route('/register')
+#def register():
+#    return render_template('register.html')
+
+# Route for user signup
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    # Create dict of the request data
+    data = request.form
+    # Get the data form; name, email and passowrd
+    name, email = data.get('name'), data.get('email')
+    password = data.get('password')
+
+    if not name or not email or not password:
+        # Return 400 if the data is incomplete
+        return make_response('Missing data', 400)
+
+    # Check if the data is complete/User provided all data
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        # Create a new user with database ORM and save
+        new_user = User(
+            public_id=str(uuid.uuid4()),
+            name=name,
+            email=email,
+            password=generate_password_hash(password, method='pbkdf2:sha256')
+        )
+        # Save the user
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Return 201 if user is created
+        return make_response('Successfully registered', 201)
+    else:
+        # Return 202 if the user is already created
+        return make_response('User already exists. Please Log in.', 202)
+
+
 @app.before_request
 def check_content_type():
-    if request.method in ["POST", "PUT", "PATCH", "DELETE"] and request.headers["Content-Type"] != "application/json":
+    if request.method in ["POST", "PUT", "PATCH", "DELETE"] and request.headers.content-type != "application/json":
         return jsonify({"message": "Content-Type must be application/json"}), 400
 
 @app.teardown_appcontext
